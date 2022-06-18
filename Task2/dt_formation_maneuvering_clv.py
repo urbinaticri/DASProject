c86341914507aebaf4e0d461d5cdd9c132608451
FilippoGuarda <filippo.guarda96@gmai.com>
2022-06-18 17:38:35 +0200

    correction bb

--- Task2/dt_formation_maneuvering_clv.py
+++ Task2/dt_formation_maneuvering_clv.py
@@ -131,8 +131,6 @@ LL_ext_low = np.concatenate((LL_kron, np.zeros(LL_kron.shape)), axis = 1)
 LL_ext = np.concatenate((LL_ext_up, LL_ext_low), axis = 0)
 
 BB = np.concatenate((np.zeros_like(BB_ext_up), BB_ext_low ), axis = 0)
-print(BB)
-exit()
 
 A = -LL_ext
 B = BB
